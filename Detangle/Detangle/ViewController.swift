1d92dd3f6bd270f7d15c6e0a6b46424adc353a5b
Dave Spina <dspina79@gmail.com>
2021-04-13 20:40:36 -0400

    Basic dot placement

--- Detangle/Detangle/ViewController.swift
+++ Detangle/Detangle/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var currentLevel = 0
+    var connections = [UIView]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .darkGray
+        levelUp()
     }
 
+    func levelUp() {
+        currentLevel += 1
+        connections.forEach{ $0.removeFromSuperview() }
+        connections.removeAll()
+
+        for _ in 1...(currentLevel + 4) {
+            let connection = UIView(frame: CGRect(origin: .zero, size: CGSize(width: 44, height: 44)))
+            connection.backgroundColor = .white
+            connection.layer.cornerRadius = 22
+            connection.layer.borderWidth = 2
+            connections.append(connection)
+            
+            view.addSubview(connection)
+        }
+        
+        connections.forEach(place)
+    }
+    
+    func place(_ connection: UIView) {
+        let randomX = CGFloat.random(in: 20...view.bounds.maxX - 20)
+        let randomY = CGFloat.random(in: 50...view.bounds.maxY - 50)
+        
+        connection.center = CGPoint(x: randomX, y: randomY)
+    }
 
 }
 
